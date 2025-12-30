addfd7981d01071b12781f227216c5738bb9d065
Ayansh-Srivastava-2006 <ayanshsrivastava30042006@gmail.com>
2025-12-30 23:58:53 +0530

    final commit

--- app/src/main/java/com/example/online_personal_finance_manager/api/ApiClient.java
+++ app/src/main/java/com/example/online_personal_finance_manager/api/ApiClient.java
@@ -6,8 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
     // Change this to your computer's IP address when testing on a real device
     // For emulator, use 10.0.2.2 to access localhost
-    private static final String BASE_URL = "http://10.0.2.2:8080/api/";
-    
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
@@ -24,4 +24,3 @@ public class ApiClient {
         return getClient().create(ApiService.class);
     }
 }
-
